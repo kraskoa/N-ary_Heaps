29920b0275898414450cb5546a3c303bf1927990
Adam Kraś <adam.kras.stud@pw.edu.pl>
2023-04-21 07:58:03 +0200

    Basic layout for Heap3 class implementation

---
+++ heap3.py
@@ -0,0 +1,16 @@
+class Heap3:
+    def __init__(self):
+        self.heap = []
+
+    def add(self, value):
+        self.heap.append(value)
+        index = len(heap) - 1
+        parent_index = (index - 1) / 2
+        while (index > 0) and (heap[index] > heap[parent_index]):
+            pass
+
+    def remove(self, value):
+        pass
+
+    def print_heap(self):
+        pass
