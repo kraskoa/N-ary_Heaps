f2a48d0b24310a5de6272f0986f46acad63948a6
Krzysztof Król <krzysztof.krol.stud@pw.edu.pl>
2023-04-24 08:23:50 +0200

    Creating function main

---
+++ main.py
@@ -0,0 +1,21 @@
+from heap import Heap
+from graph_maker import combined_3_graph_maker
+from create_list import create_list
+
+def main():
+    heap_2_insertion_times = {}
+    heap_3_insertion_times = {}
+    heap_4_insertion_times = {}
+    heap_2_removal_times = {}
+    heap_3_removal_times = {}
+    heap_4_removal_times = {}
+
+    list = create_list(100000)
+    heap_2 = Heap(2)
+    heap_3 = Heap(3)
+    heap_4 = Heap(4)
+
+
+
+if __name__=="__main__":
+    main()
